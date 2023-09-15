72001ae92533f11613e9470f908d4abcec1f2676
notnotmelon <zacharypicco@gmail.com>
2023-09-15 07:22:49 -0500

    Pyblock fix & zh-CN translation

--- control.lua
+++ control.lua
@@ -6,7 +6,7 @@ local function init()
 end
 
 script.on_init(function()
-    if remote.interfaces['freeplay'] and not script.active_mods.Pyblock then
+    if remote.interfaces['freeplay'] and not script.active_mods.PyBlock then
         remote.call('freeplay', 'set_created_items', {})
         remote.call('freeplay', 'set_ship_items', {
             ['burner-mining-drill'] = 10,
