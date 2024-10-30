02634f4ac755ba7523302296905032c7d7280484
notnotmelon <zacharypicco@gmail.com>
2024-10-30 12:27:22 -0500

    other: Removed dependancy on "undeletable fluids" as this is not yet ported for 2.0

--- settings-updates.lua
+++ settings-updates.lua
@@ -6,7 +6,9 @@ end
 
 force_string_setting('aai-loaders-lubricant-recipe', 'string-setting', 'disabled')
 force_string_setting('aai-loaders-mode', 'string-setting', 'lubricated')
+--[[
 force_string_setting('undeletable_fluids_flush_action', 'string-setting', 'prevent')
 force_string_setting('undeletable_fluids_removal_action', 'string-setting', 'prevent')
 force_string_setting('undeletable_fluids_destruction_action', 'string-setting', 'prevent')
 force_string_setting('undeletable_fluids_minimum_threshold', 'int-setting', 200)
+--]]
