5bf47a7f1e36444b365eb304421c930a9de3b212
NeveHanter <nevehanter@gmail.com>
2024-12-06 20:14:27 +0100

    Add basic mod settings

---
+++ settings.lua
@@ -0,0 +1,26 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "pyhm-decrease-containers-inventory-size",
+        default_value = true,
+        setting_type = "startup"
+    },
+    {
+        type = "bool-setting",
+        name = "pyhm-replace-sinkhole-with-outfall",
+        default_value = true,
+        setting_type = "startup"
+    },
+    {
+        type = "bool-setting",
+        name = "pyhm-resource-rocks-disallow-building-over",
+        default_value = true,
+        setting_type = "startup"
+    },
+    {
+        type = "bool-setting",
+        name = "pyhm-resource-rocks-infinite",
+        default_value = true,
+        setting_type = "startup"
+    }
+})
